b2c4a313940ea7a4e3854df9802652b73675d410
sergey.glazov <sergey.glazov@kitsoft.kiev.ua>
2017-06-15 20:51:37 +0300

    include react into a project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 /**
  * Created by Jesster on 15.06.2017.
  */
+
+console.log(React);
+console.log(ReactDOM);
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 /**
  * Created by Jesster on 15.06.2017.
  */
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(__dirname));
+
+app.listen(app.get('port'), function() {
+    console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
